391d558594929b1a240fb9bcbb14e2ba56374914
santhiya3618 <24203033@srcas.ac.in>
2025-07-14 12:09:36 +0530

    Update testt.java

--- testt.java
+++ testt.java
@@ -1,1 +1,8 @@
-
+public class program2{
+  public static void main(String[]args){
+  System.out.println("welcome");
+  System.out.println("to");
+  System.out.println("Devops");
+  System.out.println("class");
+}
+}
